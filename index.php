3a8b1299373766214919df8a96f3a9e5b648ed08
Dwi Yuliarto <31852068+dwi-yuliarto@users.noreply.github.com>
2019-06-25 17:44:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "tcp:dwidicodingserver.database.windows.net, 1433";//<Nama server database Anda>
+    $host = "dwidicodingserver.database.windows.net";//<Nama server database Anda>
     $user = "dwidicoding@dwidicodingserver"; //<Nama admin database Anda>
     $pass = "Ilona200812Alber"; //<Password admin database Anda>
     $db = "dwidicoding_db"; //<Nama database Anda>
